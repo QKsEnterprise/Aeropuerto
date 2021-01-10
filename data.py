ea3f8d0ba2ec6087f56b25152e2efc37e0d8f1dc
Kuka <martinelliluca@gmail.com>
2021-01-10 20:31:20 -0300

    Informacion de los pasajeros

---
+++ data.py
@@ -0,0 +1,39 @@
+JonnyCarter = [
+    "Name: Jonny",
+    "UsserName: Charter",
+    "DNI: 20.192675",
+    "NumberPhone: 15-336952"
+]
+
+print("Jonny Carter")
+print(JonnyCarter)
+
+ByllyCarter = [
+    "Name: Billy",
+    "Ussername: Charter",
+    "DNI: 20.122349",
+    "numberPhone: 30-453675"
+]
+
+print("Billy Carter")
+print(ByllyCarter)
+
+JonhCarter = [
+    "Name: Jonh",
+    "UsserName: Charter",
+    "DNI: 20.183947",
+    "NumberPhone: 34-837937"
+]
+
+print("Jonh Carter")
+print(JonhCarter)
+
+MarthaCarter = [
+    "Name: Martha",
+    "UsserName: Charter",
+    "DNI: 20.192673",
+    "NumberPhone: 12-374924"
+]
+
+print("Martha Carter")
+print(MarthaCarter)
