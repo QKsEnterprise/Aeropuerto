f27c14a5057334a7f4439e9e78fc7d283d0aa41c
Kuka <martinelliluca@gmail.com>
2021-01-10 11:38:18 -0300

    Optiones para vuelos

---
+++ vuelo.py
@@ -0,0 +1,13 @@
+options = ("España", "Francia", "Portugal", "Russia", "EE.UU", "England")
+
+# print(options)
+countries = [
+    "España",
+    "Francia",
+    "Portugal",
+    "Russia",
+    "EE.UU",
+    "England"
+]
+
+print(countries)
